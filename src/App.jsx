646467d4222949a91edd68ae5a69210141b7e9cb
ARVIND M <126601502+Arvind22cse@users.noreply.github.com>
2024-06-16 14:14:44 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from './Button'; // Corrected import to named import
 import Student from './Student';
+import Login from './Login';
 function App() {
     return (
         <>
+            <Login isLoggedIn={true} username="Arvind"/>
             <Student name="Arvind" age="20" isStudent={true}/>
             <Student name="TamilSelvan" age={70} isStudent={false}/>
             <Student name="Bharath" age={20} isStudent={true}/>
---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+function Login(props){
+   
+//    if(props.isLoggedIn){
+//     return(<h1>Welcome {props.username}</h1>)
+//    }
+//    else{
+//     return <h1>Please login to continue</h1>
+//    }
+const welcome=<h2 className="wel_msg">Welcome {props.username}</h2>;
+const error=<h2 className="error_msg">Please login to continue</h2>;
+return(props.isLoggedIn ? welcome:error);
+}
+export default Login;
